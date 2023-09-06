378a1ff799c944e3676e209cdc8850112514e5e3
mkilian <kilian.moun@ynov.com>
2023-09-06 19:40:31 +0200

    Test5

--- isnegative.go
+++ isnegative.go
@@ -1,21 +1,13 @@
-package main
+package piscine
 
-import (
-	"piscine"
-
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
 	if nb < 0 {
-		z01.PrintRune(54)
+		z01.PrintRune(84)
+		z01.PrintRune('\n')
 	} else {
 		z01.PrintRune(70)
+		z01.PrintRune('\n')
 	}
-}
-
-func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
 }
