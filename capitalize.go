fff47447ea80ad8c63e207d30903b813c1ca6f7d
mkilian <kilian.moun@ynov.com>
2023-09-11 11:12:06 +0200

    youpi

--- capitalize.go
+++ capitalize.go
@@ -8,7 +8,7 @@ func Capitalize(s string) string {
 		if tab[i] >= 'a' && tab[i] <= 'z' {
 			if (tab[i-1] >= 'A' && tab[i-1] <= 'Z') || (tab[i-1] >= 'a' && tab[i-1] <= 'z') {
 				result = result + string(tab[i])
-			} else if tab[i-1] == ' ' || tab[i-1] == '+' || tab[i-1] == '!' || tab[i-1] == '?' || tab[i-1] >= 48 && tab[i-1] <= 57 {
+			} else if tab[i-1] == ' ' || tab[i-1] == '+' || tab[i-1] == '!' || tab[i-1] == '?' || (tab[i-1] >= 48 && tab[i-1] <= 57) || tab[i-1] == '_' || tab[i-1] == '@' || tab[i-1] == '%' {
 				result = result + string(rune(tab[i]-32))
 			}
 		} else {
