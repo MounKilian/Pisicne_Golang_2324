04a7dcffb0679c213ff9b9eee5007a59e4ffe3ed
mkilian <kilian.moun@ynov.com>
2023-09-11 17:02:10 +0200

    youpiiiiiiiiiiiiiiiiiiiiiiii

---
+++ trimatoi.go
@@ -0,0 +1,56 @@
+package piscine
+
+func TrimAtoi(s string) int {
+	i := 0
+	for _, j := range s {
+		if j >= '0' && j <= '9' {
+			if j == '0' {
+				i += 0
+				i *= 10
+			} else if j == '1' {
+				i += 1
+				i *= 10
+			} else if j == '2' {
+				i += 2
+				i *= 10
+			} else if j == '3' {
+				i += 3
+				i *= 10
+			} else if j == '4' {
+				i += 4
+				i *= 10
+			} else if j == '5' {
+				i += 5
+				i *= 10
+			} else if j == '6' {
+				i += 6
+				i *= 10
+			} else if j == '7' {
+				i += 7
+				i *= 10
+			} else if j == '8' {
+				i += 8
+				i *= 10
+			} else if j == '9' {
+				i += 9
+				i *= 10
+			}
+		}
+	}
+	num := i / 10
+	if s[0] == '-' {
+		return -num
+	}
+	aide := 0
+	for _, j := range s {
+		if j >= '0' && j <= '9' {
+			aide++
+		}
+		if j == '-' {
+			if aide == 0 {
+				num = -num
+			}
+		}
+	}
+	return num
+}
