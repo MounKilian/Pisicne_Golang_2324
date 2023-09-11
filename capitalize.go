6ccdd72a285b2e0940d796f9d13b654d71130cbd
mkilian <kilian.moun@ynov.com>
2023-09-11 11:10:26 +0200

    youpi

--- capitalize.go
+++ capitalize.go
@@ -3,14 +3,16 @@ package piscine
 func Capitalize(s string) string {
 	var result string
 	tab := []rune(s)
-	result = string(tab[0])
+	result = result + string(tab[0])
 	for i := 1; i <= len(tab)-1; i++ {
-		if tab[i] >= 97 && tab[i] <= 122 {
-			if tab[i-1] <= 97 || tab[i-1] >= 122 {
+		if tab[i] >= 'a' && tab[i] <= 'z' {
+			if (tab[i-1] >= 'A' && tab[i-1] <= 'Z') || (tab[i-1] >= 'a' && tab[i-1] <= 'z') {
 				result = result + string(tab[i])
+			} else if tab[i-1] == ' ' || tab[i-1] == '+' || tab[i-1] == '!' || tab[i-1] == '?' || tab[i-1] >= 48 && tab[i-1] <= 57 {
+				result = result + string(rune(tab[i]-32))
 			}
 		} else {
-			result = result + string(tab[i]-32)
+			result = result + string(tab[i])
 		}
 	}
 	return result
