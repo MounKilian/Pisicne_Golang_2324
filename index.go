353517c2dfc6cfceb04952ee279b5ccad592b57c
mkilian <kilian.moun@ynov.com>
2023-09-12 12:47:13 +0200

    t

--- index.go
+++ index.go
@@ -1,7 +1,29 @@
 package piscine
 
-import "strings"
-
 func Index(s string, toFind string) int {
-	return (strings.Index(s, toFind))
+	plus := 0
+	count := 0
+	tab := []rune(toFind)
+	tab2 := []rune(s)
+	for _, i := range s {
+		if i == tab[0] {
+			break
+		} else {
+			count++
+			plus++
+		}
+	}
+	if count == len(s) {
+		return -1
+	}
+	if count != 0 {
+		for _, j := range tab {
+			if tab2[plus] == j {
+				plus++
+			} else {
+				return -1
+			}
+		}
+	}
+	return count
 }
