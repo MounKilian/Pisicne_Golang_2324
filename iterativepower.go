43f7b16bdbea39d5a3c66ebce35f9dc744ce6150
mkilian <kilian.moun@ynov.com>
2023-09-13 09:51:52 +0200

    hehe

--- iterativepower.go
+++ iterativepower.go
@@ -4,8 +4,10 @@ func IterativePower(nb int, power int) int {
 	result := nb
 	if nb == 0 {
 		return 0
-	} else if power <= 0 {
+	} else if power < 0 {
 		return 0
+	} else if power == 0 {
+		return 1
 	} else {
 		for i := 1; i <= power-1; i++ {
 			nb *= result
