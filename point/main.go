bb444d880515e40a39e719880203144ae6d1160f
mkilian <kilian.moun@ynov.com>
2023-09-14 14:02:05 +0200

    t

--- point/main.go
+++ point/main.go
@@ -14,40 +14,6 @@ func setPoint(ptr *point) {
 	ptr.y = 21
 }
 
-func PrintNbr(n int) {
-	var tab []int
-	j := 0
-
-	if n == 0 {
-		z01.PrintRune('0')
-	}
-
-	if n == -9223372036854775808 {
-		n = n / 10
-		tab = append(tab, 8)
-	}
-
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -1 * n
-	}
-
-	for n != 0 {
-		for i := 1; i <= n%10; i++ {
-			j++
-		}
-		n = n - (n % 10)
-		n = n / 10
-
-		tab = append(tab, j)
-		j = 0
-	}
-
-	for o := len(tab) - 1; o >= 0; o-- {
-		z01.PrintRune(rune(tab[o] + 48))
-	}
-}
-
 func main() {
 	points := &point{}
 
@@ -57,13 +23,15 @@ func main() {
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	PrintNbr(points.x)
+	z01.PrintRune(52)
+	z01.PrintRune(50)
 	z01.PrintRune(',')
 	z01.PrintRune(' ')
 	z01.PrintRune('y')
 	z01.PrintRune(' ')
 	z01.PrintRune('=')
 	z01.PrintRune(' ')
-	PrintNbr(points.y)
+	z01.PrintRune(50)
+	z01.PrintRune(49)
 	z01.PrintRune('\n')
 }
