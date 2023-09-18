797c756fe7c4d3b51a672c8b5eded281b0427dda
mkilian <kilian.moun@ynov.com>
2023-09-18 13:20:08 +0200

    m

--- appendrange.go
+++ appendrange.go
@@ -2,7 +2,7 @@ package piscine
 
 func AppendRange(min, max int) []int {
 	result := []int{}
-	if max <= min {
+	if max <= min || max == 0 || min == 0 {
 		return result
 	}
 	for i := min; i <= max; i++ {
