3a650799bdc9ea77086b465eb86a03985ca2a245
mkilian <kilian.moun@ynov.com>
2023-09-19 11:57:44 +0200

    m

--- split.go
+++ split.go
@@ -3,14 +3,11 @@ package piscine
 func Split(s, sep string) []string {
 	result := []string{}
 	aide := ""
-	for i := 0; i <= len(s)-2; i++ {
+	for i := 0; i <= len(s)-len(sep); i++ {
 		if s[i:i+len(sep)] == sep {
 			result = append(result, aide)
 			aide = ""
 			i = i + len(sep) - 1
-			if i >= len(s)-2 {
-				i = len(s) - 3
-			}
 		} else {
 			aide += string(s[i])
 		}
