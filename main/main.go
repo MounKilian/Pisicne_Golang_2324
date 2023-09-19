eb19c53f1b9aa1c718d1c63827cc69259a40b792
mkilian <kilian.moun@ynov.com>
2023-09-19 11:08:41 +0200

    m

--- main/main.go
+++ main/main.go
@@ -1,10 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"piscine"
-)
+import "piscine"
 
 func main() {
-	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("LL&wX<F(+}:P| b(^5;]:;6w1T0 09Wn5)~v}.T#+ :E{9zr=bGRvrf of*'VlMV7LD<g f:P;bHXU8_fyz JS>~lCDR^~,8$ (CmDqFy#x7g< "))
+	a := piscine.SplitWhiteSpaces("Hello how are you?")
+	piscine.PrintWordsTables(a)
 }
