a5fc0fb66ba04f54292e4b516425dc32f4e929d2
mkilian <kilian.moun@ynov.com>
2023-09-19 10:30:18 +0200

    m

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -3,9 +3,13 @@ package piscine
 func SplitWhiteSpaces(s string) []string {
 	result := []string{}
 	aide := ""
-	for _, i := range s {
+	for j, i := range s {
 		if string(i) == " " {
-			result = append(result, aide)
+			if string(s[j-1]) == " " {
+				result = append(result, "")
+			} else {
+				result = append(result, aide)
+			}
 			aide = ""
 		} else {
 			aide += string(i)
