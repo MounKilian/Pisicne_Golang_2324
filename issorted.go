3e32a45171246308340cf0b34304a37b3745c6ab
mkilian <kilian.moun@ynov.com>
2023-09-21 10:12:18 +0200

    m

--- issorted.go
+++ issorted.go
@@ -3,8 +3,17 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	result := true
 	etat := 1
+	count := 0
 	if f(a[0], a[1]) < 0 {
 		etat = 0
+	}
+	for j := 0; j <= len(a)-2; j++ {
+		if f(a[j], a[j+1]) == 0 {
+			count++
+		}
+	}
+	if count == len(a)-1 {
+		return true
 	}
 	for i := 1; i <= len(a)-2; i++ {
 		if etat == 0 {
--- main/main.go
+++ main/main.go
@@ -17,7 +17,7 @@ func f(a, b int) int {
 
 func main() {
 	a1 := []int{893348, 750835, 381939, 231675, -338395, -417710, -435701, -944974}
-	a2 := []int{0, 2, 1, 3}
+	a2 := []int{0, 0, 0, 0, 0, 0, 0}
 
 	result1 := piscine.IsSorted(f, a1)
 	result2 := piscine.IsSorted(f, a2)
