7a53b5463470e800176f62a1e78f9513fab8d1b1
mkilian <kilian.moun@ynov.com>
2023-09-24 19:40:54 +0200

    m

---
+++ ztail/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	arg := os.Args[1:]
+	if len(arg) == 1 {
+		data, err := os.ReadFile(arg[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Print(string(data))
+	} else if len(arg) > 1 {
+		fmt.Print("Too many arguments")
+		fmt.Print("\n")
+	} else {
+		fmt.Print("File name missing")
+		fmt.Print("\n")
+	}
+}
