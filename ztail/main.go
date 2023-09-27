81d0eb84bcc5fd2ce229101bb43966510d059430
mkilian <kilian.moun@ynov.com>
2023-09-27 10:35:53 +0200

    m

--- ztail/main.go
+++ ztail/main.go
@@ -63,6 +63,7 @@ func main() {
 					fmt.Print(string(data[n]))
 				}
 				os.Exit(1)
+
 			} else {
 				fmt.Println("==>", arg[m], "<==")
 				for k := len(data) - help + 1; k <= len(data)-1; k++ {
