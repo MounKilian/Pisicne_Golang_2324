e6fc541f2f3de835ac78a3bb2f865a38cda9df68
mkilian <kilian.moun@ynov.com>
2023-09-27 12:43:00 +0200

    m

--- main/main.go
+++ main/main.go
@@ -6,5 +6,15 @@ import (
 )
 
 func main() {
-	fmt.Printf("%#v\n", piscine.SplitWhiteSpaces("Hello how are you?"))
+
+	link := &piscine.List{}
+
+	piscine.ListPushBack(link, "Hello")
+	piscine.ListPushBack(link, "man")
+	piscine.ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
 }
