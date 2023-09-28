738a519c0c7e14c70ae3d3bd27d79fca82377f98
mkilian <kilian.moun@ynov.com>
2023-09-28 09:15:14 +0200

    quest 1 ex 1

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -1,10 +1,21 @@
+// package main
+
+// import "github.com/01-edu/z01"
+
+// func main() {
+// 	for i := 97; i <= 122; i++ {
+// 		z01.PrintRune(rune(i))
+// 	}
+// 	z01.PrintRune('\n')
+// }
+
 package main
 
 import "github.com/01-edu/z01"
 
 func main() {
-	for i := 97; i <= 122; i++ {
-		z01.PrintRune(rune(i))
+	for i := 'a'; i <= 'z'; i++ {
+		z01.PrintRune(i)
 	}
 	z01.PrintRune('\n')
 }
